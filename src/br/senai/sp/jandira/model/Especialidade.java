d12d2563e7e6f85e7cadbe3960551552a00b051f
Xulia <juliasoaresdealmeidaj@gmail.com>
2022-10-04 13:19:03 -0300

    Atualização

--- src/br/senai/sp/jandira/model/Especialidade.java
+++ src/br/senai/sp/jandira/model/Especialidade.java
@@ -3,50 +3,64 @@ package br.senai.sp.jandira.model;
 import javax.swing.JOptionPane;
 
 public class Especialidade {
-	
-	private String nome;
-	private String descricao;
-	
-	//construtor
-	
-	public Especialidade(String nome) {
-		this.nome = nome;
-	}
-	
-	public Especialidade(String nome, String descricao) {
-		
-	}
-	
-	public Especialidade() {
-		//Contrutor default.
-	}
-	
-	
-	//Métodos de acessos aos atributos
-	
-	public void setNome(String nome) {
-		if(nome.length() >= 3) {
-		this.nome = nome;
-		} else {
-			JOptionPane.showMessageDialog(null, nome + " não é um nome válido.\nDeve conter pelo menos 3 letras!", "Erro", JOptionPane.ERROR_MESSAGE);
-		}	
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setDescricao(String descricao) {
-		if(descricao.length() >= 10) {
-		this.descricao = descricao;
-		} else {
-			JOptionPane.showMessageDialog(null, "A descrição deve conter pelo menos 10 caracteres!", "Erro", JOptionPane.ERROR_MESSAGE);
-		}
-	}
-	
-	public String getDescricao() {
-		return descricao;
-	} 
-	
+
+    private Integer codigo;
+    private static int contador;
+    private String nome;
+    private String descricao;
+
+    //construtor
+    public Especialidade(String nome) {
+        this.nome = nome;
+        this.contador++;
+        this.codigo = contador;
+    }
+
+    public Especialidade(String nome, String descricao) {
+        this.contador++;
+        this.codigo = contador;
+    }
+
+    public Especialidade() { //Contrutor default.
+        this.contador++;
+        this.codigo = contador;
+    }
+
+    //Métodos de acessos aos atributos
+
+    public static int getContador() {
+        return contador;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+    
+    
+    
+    
+    public void setNome(String nome) {
+        if (nome.length() >= 3) {
+            this.nome = nome;
+        } else {
+            JOptionPane.showMessageDialog(null, nome + " não é um nome válido.\nDeve conter pelo menos 3 letras!", "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setDescricao(String descricao) {
+        if (descricao.length() >= 10) {
+            this.descricao = descricao;
+        } else {
+            JOptionPane.showMessageDialog(null, "A descrição deve conter pelo menos 10 caracteres!", "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 
 }
